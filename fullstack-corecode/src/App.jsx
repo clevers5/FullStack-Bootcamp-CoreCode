b0c89e06b6742e0edf308d0504df299be5ecd22e
Clever <arnez.4292@gmail.com>
2022-11-30 17:30:58 -0400

    ReactSesion

---
+++ fullstack-corecode/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Count from './components/Count';
+//import EffectsExample from './components/EffectsExample';
+//import Formulario from './components/Formulario';
+//import FormularoFormik from './components/FormularoFormik';
+import Dynamic from './components/Dynamic';
+function App() {
+
+  return (
+    <div>
+      {<Count/>}
+      {/*<EffectsExample/>*/}
+      {/*<Formulario/>*/}
+      {/*<FormularoFormik/>*/}
+      {<Dynamic/>}
+    </div>
+  );
+}
+
+export default App;
